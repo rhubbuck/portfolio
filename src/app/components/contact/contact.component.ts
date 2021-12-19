c96a29894011e9294b568a17c65f90ce228866fb
Ryan Hubbuck <rhubbuck@gmail.com>
2021-12-19 08:11:37 -0500

    project section

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from 'src/assets/contact';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  model = new Contact('Ryan', 'rhubbuck@gmail.com', 'Test message');
+
+  submitted = false;
+
+  onSumbit() {
+    this.submitted = true;
+  }
 
   constructor() { }
 
