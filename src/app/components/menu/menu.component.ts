122392be5250c7c205093a83c54e74af98ad13cc
Ryan Hubbuck <rhubbuck@gmail.com>
2022-02-06 08:28:12 -0500

    mobile version

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -13,6 +13,32 @@ export class MenuComponent implements OnInit {
     this.closeMenu.emit(false)
   }
 
+  
+  scrollToAbout() {
+    window.scroll({
+      top: 540,
+      behavior: 'smooth'
+    });
+    this.closeMenu.emit(false);
+  }
+
+  scrollToProjects() {
+    window.scroll({
+      top: 1380,
+      behavior: 'smooth'
+    });
+    this.closeMenu.emit(false);
+  }
+
+  scrollToContact() {
+    window.scroll({
+      top: 2700,
+      behavior: 'smooth'
+    });
+    this.closeMenu.emit(false);
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
